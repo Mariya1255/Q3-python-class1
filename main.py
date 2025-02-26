fb0ad21c5af16b3b833dcdb1dcfa876b2067e138
Mariya1255 <162633244+Mariya1255@users.noreply.github.com>
2025-02-27 01:59:17 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# First Class Hello World
+print("Hello, World!")
+
+# -------------------------------
+
+# Pycache Example
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Mariya"))
+
+# -------------------------------
+
+# data types
+
+# string
+name = "Mariya"
+
+# integer
+age = 24
+
+# float
+height = 5.3
+
+# boolean
+is_student = True
+
+# list
+grades = [95, 83, 90, 87, 74]
+
+# tuple
+coordinates = (3, 5)
+
+# dictionary
+person = {"name": "Mariya", "age": 24, "height": 5.3}
+
+# set
+unique_numbers = {2, 4, 6, 8, 10}
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
---
+++ module.py
@@ -0,0 +1,11 @@
+from main import greet
+print(greet("Hello world from module.py"))
+
+# -----------
+
+"""
+when we run `module.py` file, it will execute the code from the `main.py` file, and 
+This `_pycache_` folder contains the compiled version of the `main.py` file.
+"""
+
+# -----------
